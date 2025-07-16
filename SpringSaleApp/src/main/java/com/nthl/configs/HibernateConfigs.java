1873a68ad2073ff0178b1873dfc73009f59a558a
unknown <longmap2003@gmail.com>
2025-07-16 14:21:58 +0700

    base.html and sql file

--- SpringSaleApp/src/main/java/com/nthl/configs/HibernateConfigs.java
+++ SpringSaleApp/src/main/java/com/nthl/configs/HibernateConfigs.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+    https://github.com/duonghuuthanh/IT2201SpringSaleApp
  */
 package com.nthl.configs;
 
